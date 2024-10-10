23cff9b6966f50374d09ec0094c86b2ef852e5f4
Leo Feradero Nugraha <leoferaderonugraha@gmail.com>
2024-10-11 03:15:28 +0700

    Add custom domain to deployment

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 npm run build &&
 touch dist/.nojekyll &&
+cp CNAME dist/ &&
 git add -f dist &&
 git commit -m "Build the latest version" &&
 git push origin `git subtree split --prefix dist main`:gh-pages --force
