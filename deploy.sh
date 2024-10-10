bb5dacf44ffac7e720fa05c47d4e8f0e8a771ef0
Leo Feradero Nugraha <leoferaderonugraha@gmail.com>
2024-10-11 03:04:41 +0700

    Force push changes on deployment branch

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 npm run build &&
 git add -f dist &&
 git commit -m "Build the latest version" &&
-git subtree push --prefix dist origin gh-pages
+git push origin `git subtree split --prefix dist main`:gh-pages --force
